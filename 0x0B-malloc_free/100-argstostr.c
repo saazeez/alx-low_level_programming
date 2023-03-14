292215335aa041d25a476857c9e2a0a737187090
Abdulazeez Sani <sabdulazeez89@gmail.com>
2023-03-14 12:48:18 -0700

    it is the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,9 +21,9 @@ char *argstostr(int ac, char **av)
 	for (arg = 0; arg < ac; arg++)
 	{
 		for (byte = 0; av[arg][byte]; byte++)
-			size++
+			size++;
 	}
-	str = maloc(sizeof(char) * size + 1);
+	str = malloc(sizeof(char) * size + 1);
 
 	if (str == NULL)
 		return (NULL);
