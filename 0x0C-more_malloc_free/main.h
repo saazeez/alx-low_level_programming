9dfb7d4629f27b95b1c31cc825bdaf92d71b84ed
Abdulazeez Sani <sabdulazeez89@gmail.com>
2023-03-17 14:26:27 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <limits.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
